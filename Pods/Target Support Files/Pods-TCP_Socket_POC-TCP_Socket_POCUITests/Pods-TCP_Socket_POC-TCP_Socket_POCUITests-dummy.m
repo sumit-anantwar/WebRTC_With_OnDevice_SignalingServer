9191416f741b899c04d1f49d71f5d8728dcd47f1
Sumit Anantwar <sumit.anantwar@gmail.com>
2020-01-22 19:55:15 +0200

    gitignore fix

--- Pods/Target Support Files/Pods-TCP_Socket_POC-TCP_Socket_POCUITests/Pods-TCP_Socket_POC-TCP_Socket_POCUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TCP_Socket_POC_TCP_Socket_POCUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TCP_Socket_POC_TCP_Socket_POCUITests
-@end
--- Pods/Target Support Files/Pods-TCP_Socket_POC/Pods-TCP_Socket_POC-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TCP_Socket_POC : NSObject
-@end
-@implementation PodsDummy_Pods_TCP_Socket_POC
-@end
